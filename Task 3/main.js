21270f533153ae3939d8ca63275010dfa2a3c702
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2023-08-01 14:31:10 +0300

    modify Tasks files

--- Task 3/main.js
+++ Task 3/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function addHtmlElement(numberOfElement,elementId) {
     var container = ""
     for (let i = 0; i < numberOfElement; i++) {
@@ -26,32 +25,3 @@ addHtmlImage("insert4","images/Students-4.jpg","Students","400","400")
 
 
 
-=======
-function addHtmlElement(numberOfElement,elementId) {
-    var container = ""
-    for (let i = 0; i < numberOfElement; i++) {
-        container += "<h2>Abdelrahman Salah</h2>"
-    }
-    document.getElementById(elementId).innerHTML = container
-}
-
-addHtmlElement(5,"insert1")
-addHtmlElement(2,"insert2")
-addHtmlElement(3,"insert3")
-addHtmlElement(4,"insert4")
-
-
-function addHtmlImage(elementId,imageSource,alt,imageWidth,imageHeight) {
-    var container = ""
-    container += "<img src=" + imageSource + " alt=" + alt + " width=" + imageWidth + " height=" + imageHeight + ">"
-    document.getElementById(elementId).innerHTML = container
-}
-
-addHtmlImage("insert1","images/Students-1.jpg","Students","400","400")
-addHtmlImage("insert2","images/Students-2.jpg","Students","400","400")
-addHtmlImage("insert3","images/Students-3.jpg","Students","400","400")
-addHtmlImage("insert4","images/Students-4.jpg","Students","400","400")
-
-
-
->>>>>>> 227d3e6a0e681edc3e621bd08920807db25e7f45
